3f84ac2e30c8d157189f89fe37a35e7c700b09b4
chestahedron <kilian.nero@gmail.com>
2023-06-07 16:05:40 +1000

    barba init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import barba from '@barba/core';
+import { gsap } from 'gsap';
 
-barba.init();
+barba.init({
+  transitions: [
+    {
+      name: 'animate',
+      async leave(data) {
+        await gsap.to(data.current.container, { opacity: 0, duration: 0.3 });
+        //console.log(data);
+      },
+      async enter(data) {
+        await gsap.to(data.next.container, { opacity: 1, duration: 0.3 });
+        //console.log(data);
+      },
+    },
+  ],
+});
